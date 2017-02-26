84e5dfc191e9288791aea20f14c57d0b8b3d8c55
Tom Read Cutting <tr395@cam.ac.uk>
2017-02-26 17:57:53 +0000

    Port perl example from question 6

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -1,5 +1,20 @@
+""" Euclids algorithm """
 
+def gcd(a0, b0):
+    """ Greatest common divisor algorithm """
+    a = a0
+    b = b0
+    while True:
+        q = a // b
+        if a == (b * q):
+            print("gcd({}, {}) = {}".format(a0, b0, b))
+            return b
+        (a, b) = (b, a - (b * q))
+
+
+def main():
+    """ The main method """
+    gcd(2250, 360)
 
 if __name__ == "__main__":
-    
-    
+    main()
