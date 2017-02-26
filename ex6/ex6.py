5b9c14d2ba44359ac82c5a6940f9bc710b1f0619
Tom Read Cutting <tr395@cam.ac.uk>
2017-02-26 18:14:14 +0000

    Complete ex6 part c

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -2,15 +2,16 @@
 
 def gcd(a0, b0):
     """ Greatest common divisor algorithm """
-    a = a0
-    b = b0
+    (a, b) = (a0, b0)
+    (aa, ab) = (1, 0)
+    (ba, bb) = (0, 1)
     while True:
         q = a // b
         if a == (b * q):
-            print("gcd({}, {}) = {}".format(a0, b0, b))
+            print("gcd({}, {}) = {} = {} * {} + {} * {}".format(a0, b0, b, a0, ba, b0, bb))
             return b
         (a, b) = (b, a - (b * q))
-
+        (aa, ab, ba, bb) = (ba, bb, aa - (q * ba), ab - (q * bb))
 
 def main():
     """ The main method """
